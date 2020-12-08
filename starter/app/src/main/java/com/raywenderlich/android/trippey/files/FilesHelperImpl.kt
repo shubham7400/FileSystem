37611fcfffa30538eb4782837d179eaaa56bed55
shubham mogarkar <shubhammogarkar74@gmail.com>
2020-12-08 16:13:38 +0530

    it is project about file system

---
+++ starter/app/src/main/java/com/raywenderlich/android/trippey/files/FilesHelperImpl.kt
@@ -0,0 +1,38 @@
+package com.raywenderlich.android.trippey.files
+
+import java.io.File
+import java.io.FileOutputStream
+
+class FilesHelperImpl(private val directory: File): FilesHelper {
+    override fun saveData(fileName: String, data: String) {
+         val file = buildFile(fileName)
+        val fileOutputStream = FileOutputStream(file)
+        try {
+            fileOutputStream.use {
+                it.write(data.toByteArray())
+            }
+        }catch (error: Throwable)
+        {
+            error.printStackTrace()
+        }
+    }
+
+    override fun getData(): List<File> = directory.listFiles()?.toList() ?: emptyList()
+
+
+    override fun deleteData(fileName: String) {
+         val targetFile = buildFile(fileName)
+        if (targetFile.exists())
+        {
+            targetFile.delete()
+        }
+    }
+
+    private fun buildFile(fileName: String): File{
+        return File(directory,fileName)
+    }
+
+    private fun buildOutputStream(file: File): FileOutputStream{
+        return FileOutputStream(file)
+    }
+}
